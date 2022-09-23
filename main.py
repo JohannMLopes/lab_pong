bdb7998160e2409d586412c7aaf8bf09073eb204
Johann <49884878+JohannMLopes@users.noreply.github.com>
2022-09-23 10:53:59 -0300

    segunda bola spawna (undone)

--- main.py
+++ main.py
@@ -1,39 +1,70 @@
 from PPlay.window import *
 from PPlay.sprite import *
 
-#Inicia recursos
+# Inicia recursos
 
 janela = Window(800, 600)
-janela.set_title("Pong")
+janela.set_title("Pong_Lab")
 
 teclado = Window.get_keyboard()
 
-velx = 180
-vely = 180
+b1x = 100
+b1y = 100
 
-velp = 180
+b2x = 0
+b2y = 0
+
+velp = 100
 
 pont1 = 0
 pont2 = 0
 
+# contador colisao da bola1
+contb = 0
+
+# Esconde as bolinhas
+
+hideb1 = False
+hideb2 = True
+
 #Inicia GameObjects
 
-bola = Sprite("images/bola.png", 1)
-bola.x = (janela.width/2) - (bola.width/2)
-bola.y = (janela.height/2) - (bola.height/2)
+bola1 = Sprite("images/bola.png")
+bola1.x = (janela.width/2) - (bola1.width/2)
+bola1.y = (janela.height/2) - (bola1.height/2)
 
-pad1 = Sprite("images/barra.png", 1)
+bola2 = Sprite("images/bola.png")
+bola2.x = (janela.width/2) - (bola2.width/2)
+bola2.y = (janela.height/2) - (bola2.height/2)
+
+pad1 = Sprite("images/barra.png")
 pad1.x = 10
-pad1.y = janela.height/2 - pad1.height/2
+pad1.y = (janela.height/2) - (pad1.height/2)
 
 pad2 = Sprite("images/barra.png")
-pad2.x = janela.width - pad1.width - 10
-pad2.y = janela.height/2 - pad2.height/2
+pad2.x = janela.width - pad2.width - 10
+pad2.y = (janela.height/2) - (pad2.height/2)
 
-#GameLoop
-while True:
+# GameLoop
+while 1:
 
-    #Movimento Pad
+    # Verifica o estado das bolinhas
+    if hideb1 and hideb2:
+        hideb1 = False
+        hideb2 = False
+        contb = 0
+        b1x = 100
+        b1y = 100
+        b2x = -100
+        b2y = -100
+
+    if contb == 3:
+        hideb2 = False
+        b2x = -100
+        b2y = -100
+        contb += 1
+
+    # Movimento pad
     if pad1.y < 0:
         pad1.y = 0
     if pad1.y > janela.height - pad1.height:
@@ -47,44 +78,84 @@ while True:
         pad1.y -= velp * janela.delta_time()
     if teclado.key_pressed("s"):
         pad1.y += velp * janela.delta_time()
-    if teclado.key_pressed("up"):
+    if teclado.key_pressed("UP"):
         pad2.y -= velp * janela.delta_time()
-    if teclado.key_pressed("down"):
+    if teclado.key_pressed("DOWN"):
         pad2.y += velp * janela.delta_time()
 
-    #Colisao pad/bola
-    if bola.collided(pad1) and velx < 0:
-        velx = -velx
-    if bola.collided(pad2) and velx > 0:
-        velx = -velx
+    #Colisão pad/bola
 
-    #Movimento Bola
-    if bola.x > janela.width - bola.width and velx > 0:
+    if bola1.collided(pad1) and b1x < 0:
+        b1x = -b1x
+        contb += 1
+    if bola1.collided(pad2) and b1x > 0:
+        b1x = -b1x
+        contb += 1
+
+    if bola2.collided(pad1) and b1x < 0:
+        b2x = -b2x
+    if bola2.collided(pad2) and b1x > 0:
+        b2x = -b2x
+
+    # Movimento bola
+
+    if bola1.x > janela.width - bola1.width and b1x > 0:
         pont1 += 1
-        velx = -velx
-        bola.x = (janela.width / 2) - (bola.width / 2)
-        bola.y = (janela.height / 2) - (bola.height / 2)
-        pad1.y = janela.height / 2 - pad1.height / 2
-        pad2.y = janela.height / 2 - pad2.height / 2
-    if bola.x < 0 and velx < 0:
+        hideb1 = True
+        b1x = 0
+        b1y = 0
+        bola1.x = (janela.width/2) - (bola1.width/2)
+        bola1.y = (janela.height/2) - (bola1.height/2)
+
+    if bola1.x < 0 and b1x < 0:
         pont2 += 1
-        velx = -velx
-        bola.x = (janela.width / 2) - (bola.width / 2)
-        bola.y = (janela.height / 2) - (bola.height / 2)
-        pad1.y = janela.height / 2 - pad1.height / 2
-        pad2.y = janela.height / 2 - pad2.height / 2
-    if bola.y > janela.height - bola.height and vely > 0:
-        vely = -vely
-    if bola.y < 0 and vely < 0:
-        vely = -vely
+        hideb1 = True
+        b1x = 0
+        b1y = 0
+        bola1.x = (janela.width/2) - (bola1.width/2)
+        bola1.y = (janela.height/2) - (bola1.height/2)
 
-    bola.x += velx * janela.delta_time()
-    bola.y += vely * janela.delta_time()
+    if bola2.x > janela.width - bola2.width and b2x > 0:
+        pont1 += 1
+        hideb2 = True
+        b2x = 0
+        b2y = 0
+        bola2.x = (janela.width/2) - (bola2.width/2)
+        bola2.y = (janela.height/2) - (bola2.height/2)
 
+    if bola2.x < 0 and b2x < 0:
+        pont2 += 1
+        hideb2 = True
+        b2x = 0
+        b2y = 0
+        bola2.x = (janela.width/2) - (bola2.width/2)
+        bola2.y = (janela.height/2) - (bola2.height/2)
 
-    #Imprime na tela
+    if bola1.y > janela.height - bola1.height and b1y > 0:
+        b1y = -b1y
+
+    if bola1.y < 0 and b1y < 0:
+        b1y = -b1y
+
+    if bola2.y > janela.height - bola2.height and b2y > 0:
+        b2y = -b2y
+
+    if bola2.y < 0 and b2y < 0:
+        b2y = -b2y
+
+    bola1.x += b1x * janela.delta_time()
+    bola1.y += b1y * janela.delta_time()
+
+    bola2.x += b2x * janela.delta_time()
+    bola2.y += b2y * janela.delta_time()
+
+    # Imprime na tela
+
     janela.set_background_color([0, 0, 0])
-    bola.draw()
+    if not hideb1:
+        bola1.draw()
+    if not hideb2:
+        bola2.draw()
     pad1.draw()
     pad2.draw()
     janela.draw_text("%d - %d" % (pont1, pont2), janela.width/2, 10, 18, (255, 255, 255), "Arial", True, False)
